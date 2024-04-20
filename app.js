ce7e4412cec04916b730d89998844bae17859204
Pramod Mahto <Mahtop324@gmail.com>
2024-04-20 13:36:25 +0545

    remove database.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mysql = require('mysql2');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mysql = require("mysql2");
 require("dotenv").config();
-
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static("public"));
 
 const pool = mysql.createPool({
   connectionLimit: 10,
@@ -16,18 +20,15 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.use(express.static('public'));
-
 const createTable = () => {
   pool.query(
-    'CREATE TABLE IF NOT EXISTS todolist (idtodolist INT AUTO_INCREMENT PRIMARY KEY, list VARCHAR(255), date DATE)',
+    "CREATE TABLE IF NOT EXISTS todolist (idtodolist INT AUTO_INCREMENT PRIMARY KEY, list VARCHAR(255), date DATE)",
     (createTableError, createTableResult) => {
       if (createTableError) {
-        console.error('Error creating todolist table:', createTableError.message);
+        console.error(
+          "Error creating todolist table:",
+          createTableError.message
+        );
         return;
       }
 
@@ -38,22 +39,22 @@ const createTable = () => {
 
 createTable();
 
-app.get('/todolist', (req, res) => {
-  pool.query('SELECT * FROM todolist', (error, rows) => {
+app.get("/todolist", (req, res) => {
+  pool.query("SELECT * FROM todolist", (error, rows) => {
     if (error) {
       console.log(error);
-      res.status(500).send('Internal Server Error');
+      res.status(500).send("Internal Server Error");
     } else {
       res.json(rows);
     }
   });
 });
 
-app.post('/todolist', (req, res) => {
+app.post("/todolist", (req, res) => {
   const { list } = req.body;
   const currentDate = new Date().toISOString().slice(0, 10);
 
-  const sql = 'INSERT INTO todolist (list, date) VALUES (?, ?)';
+  const sql = "INSERT INTO todolist (list, date) VALUES (?, ?)";
   const values = [list, currentDate];
 
   pool.query(sql, values, (error, result) => {
@@ -61,23 +62,23 @@ app.post('/todolist', (req, res) => {
       console.log(error);
       res.status(500).json({ error: "Failed to add list" });
     } else {
-      console.log('Insertion successful');
+      console.log("Insertion successful");
       res.json({ status: 200 });
     }
   });
 });
 
-app.delete('/todolist/:id', (req, res) => {
+app.delete("/todolist/:id", (req, res) => {
   const itemId = req.params.id;
 
-  const sql = 'DELETE FROM todolist WHERE idtodolist = ?';
+  const sql = "DELETE FROM todolist WHERE idtodolist = ?";
   pool.query(sql, [itemId], (error, result) => {
     if (error) {
       console.log(error);
-      res.status(500).send('Internal Server Error');
+      res.status(500).send("Internal Server Error");
     } else {
-      console.log('Deletion successful');
-      res.send('Deleted successfully');
+      console.log("Deletion successful");
+      res.send("Deleted successfully");
     }
   });
 });
